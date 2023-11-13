ecad9a60c99bd226c6400f1125dd095bb5517537
leonardo <leonardocasa790@gmail.com>
2023-11-13 17:32:41 -0300

    chore: refac read fn

--- src/utils/read.ts
+++ src/utils/read.ts
@@ -1,15 +1,10 @@
 import * as fs from "node:fs"
-import { exit } from "node:process"
 
 
 export default function read(path: string){
-   try{
-        const file = fs.readFileSync(path, 'utf-8')
-        console.log(file)
-        return file;
-   }
-   catch(err){
-        console.log(err)
-        exit()
-   }
+     const source = fs.readFileSync(path, 'utf-8')
+     console.log(source)
+     return source
 }
+
+read("test.txt")
