caae867647f054f2b243857f2530daf3d0920658
leonardo <leonardocasa790@gmail.com>
2023-11-13 17:52:06 -0300

    chore: loop  fn automato

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,15 @@
-import read from "./utils/read";
+import read from "./utils/read"
 
 
-
-enum states{
-    q0,
-    q1,
-    q2,
-    q3,
-    q4,
-    q5,
-    q6
-}
-type symbol = {
-    D: string,
-    L: string,
-    ".": string,
-    EOF: string
-}
-
-
-(
-function lexer(){
-    const code = read("test.txt")
+function afd(path: string){
+    const code = read(path);
     let index: number = 0;
 
-    while(code.length > index){
-        const currentChar = code[index];
-        const nextChar = code[index] + 1;
+    for(index; code.length > index; index++){
+        let char: string = code[index];
 
-        if(currentChar === " " || currentChar === "\n"){
-            console.log(currentChar)
-        }
-        index++
+        console.log(char+ "test")
     }
+
 }
-)()
+afd("test.txt")
--- src/utils/read.ts
+++ src/utils/read.ts
@@ -3,8 +3,5 @@ import * as fs from "node:fs"
 
 export default function read(path: string){
      const source = fs.readFileSync(path, 'utf-8')
-     console.log(source)
      return source
 }
-
-read("test.txt")
