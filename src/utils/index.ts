af35085961c09472c2d1f24a5a1e33f5cbb330af
leonardo <leonardocasa790@gmail.com>
2023-11-14 18:58:10 -0300

    chore: add fn recognize Digit

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,7 @@
 export function isSpace(char: string): boolean{
     return char === ' ' || char === '\n' || char === '\t';
 }
+
+export function isDigit(char: string): boolean{
+    return char >= '0' && char <= '9';
+}
