ba7c2e53604c7173cc58b3cf98b4ab09ad233a92
leonardo <leonardocasa790@gmail.com>
2023-11-13 22:04:22 -0300

    chore: add Alphabet

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import read from "./utils/read"
 
+enum States{
+    q0, // start
+    q1, // D+
+    q2, // .
+    q3, // D+ (fractional part)
+    q4, // error
+    q5, // recognize int
+    q6, // recognize float
+}
+
+const Alphabet = /^[0-9.]+$/;
 
 function afd(path: string){
     const code = read(path);
@@ -8,7 +19,8 @@ function afd(path: string){
     for(index; code.length > index; index++){
         let char: string = code[index];
 
-        console.log(char+ "test")
+
+        console.log(char)
     }
 
 }
