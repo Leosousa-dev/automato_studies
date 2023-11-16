6ef373233803f1155290257449010d1561162d2e
leonardo <leonardocasa790@gmail.com>
2023-11-16 13:35:52 -0300

    chore: add states

---
+++ src/types/states.ts
@@ -0,0 +1,15 @@
+
+/* 
+set of states(Q)
+Q = {q0, q1, q2, q3, q4, q5, q6}
+*/
+
+export enum States {
+   q0 = "q0",
+   q1 = "q1",
+   q2 = "q2",
+   q3 = "q3",
+   q4 = "q4",
+   q5 = "q5",
+   q6 = "q6",
+}
